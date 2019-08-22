05d743dd82d3b821c5ee974f2b45043d26536036
SaiChandra2896 <saichandra2896@gmail.com>
2019-08-22 14:02:01 +0530

    add game screen when we click start game

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from './components/Header';
 import StartGameScreen from './Screens/StartGameScreen';
+import GameScreen from './Screens/Gamescreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title='Guess a Number' />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
--- Screens/Gamescreen.js
+++ Screens/Gamescreen.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card';
 
 const generateRanddomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -17,8 +20,32 @@ const GameScreen = props => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRanddomBetween(1, 100, props.userChoice)
   );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponents Guess:</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title='LOWER' />
+        <Button title='GREATER' />
+      </Card>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignContent: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxHeight: '80%'
+  }
+});
 
 export default GameScreen;
